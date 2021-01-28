511475303b299ec800b5b7a62ab2dfb0f8571f1b
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-01-28 12:30:10 -0300

    att script

--- App06_Forca/script.js
+++ App06_Forca/script.js
@@ -5,7 +5,7 @@ let letra = ''
 
 const escolhido = bd[Math.floor(Math.random() * bd.length)];
 
-function verificaLetra(letra){
+function verificaLetra(letra, escolhido){
     if( escolhido.indexOf(letra) == -1 ){
         life-=1
     }else{
@@ -20,6 +20,8 @@ const form = document.getElementById('form')
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(letra.value)
+    console.log(escolhido)
+    verificaLetra(letra.innerHTML, escolhido)
 })
 
 
