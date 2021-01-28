0a556fbbf03f922ab65c004d5a6815ac6d0c0fef
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-01-28 01:05:02 -0300

    add function

--- App06_Forca/script.js
+++ App06_Forca/script.js
@@ -1,18 +1,19 @@
 const bd = ["Texto", "Willim", "Ricardo"
 ]
 let life = 5
-
+let letra = ''
 
 const escolhido = bd[Math.floor(Math.random() * bd.length)];
 
-console.log(escolhido)
 
 
-let letra = "a"
-if( escolhido.indexOf(letra) != -1 ){
-    console.log("Achei")
-}else{
-    console.log("Não achei")
-    life-=1
+
+
+function verificaLetra(letra){
+    if( escolhido.indexOf(letra) == -1 ){
+        life-=1
+    }else{
+        console.log("Achei")
+    }
 }
 
